e72ffa1c8f8f902c4443d665458804f034b3dc0e
cloudyBright <cloudybright@yinliangdeMac-mini.local>
2015-12-15 10:44:11 +0800

    建立初始工程
    做一个自己的ios框架，包含一些常用类库和类别以及工具，以后遇到什么好用的方法都可以加到里边

---
+++ YLKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YLKitDemo
+//
+//  Created by cloudyBright on 15/12/15.
+//  Copyright © 2015年 cloudyBright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
